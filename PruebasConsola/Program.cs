a18c8d9f554948ae7627f038a7272d172a272dee
Gerardo Alvarez Mendoza <gerardam@outlook.com>
2018-06-08 17:33:50 -0500

    Primer commit para el proyecto de pruebas en consola desde visual studio 2017

---
+++ PruebasConsola/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebasConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 4;
+            int num2 = 3;
+            int resultado = num1 + num2;
+
+            Console.WriteLine("El resultado es: "+resultado);
+            Console.ReadLine();
+        }
+    }
+}
